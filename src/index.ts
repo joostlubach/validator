be5a9cb3bad8c29a8a466274854c20b7a674943b
Joost Lubach <joostlubach@gmail.com>
2023-09-12 14:19:23 +0200

    Exporting defineType.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as ValidatorResult } from './ValidatorResult'
 export { default} from './Validator'
 export * from './typings'
 export * from './helpers'
+export { defineType } from './util'
 
 import * as types from './types'
 
