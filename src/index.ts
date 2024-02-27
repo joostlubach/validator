fcc6ced99f22cd63cd2fde510d94a4ac3ad56850
Joost Lubach <joostlubach@gmail.com>
2024-02-27 14:47:43 +0100

    Removing stray export.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as types from './types'
 
 export { default as ValidatorResult } from './ValidatorResult'
 export { default } from './Validator'
-export { deriveObjectAPISchema, openAPIDoc } from './openAPI'
+export { deriveObjectAPISchema } from './openAPI'
 export { doctext, getDocumentationFromDoctext } from './doctext'
 export * from './typings'
 export * from './helpers'
